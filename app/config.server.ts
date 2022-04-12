84a806ac792358802711960f7ca0cdd40a0b8349
Tim Habermaas <tim.habermaas@gmail.com>
2022-04-12 17:11:15 +0200

    Make event store configurable for production environment

--- app/config.server.ts
+++ app/config.server.ts
@@ -11,8 +11,10 @@ function getConfigFromEnv(): Config {
   return {
     mailSender: process.env.MAIL_SENDER === "SES" ? "SES" : "CONSOLE",
     eventStore:
-      process.env.EVENT_STORE === "s3_store" ||
-      process.env.NODE_ENV === "production"
+      process.env.EVENT_STORE === "file_store"
+        ? "file_store"
+        : process.env.EVENT_STORE === "s3_store" ||
+          process.env.NODE_ENV === "production"
         ? "s3_store"
         : "file_store",
     adminPassword: process.env.ADMIN_PASSWORD ?? "admin",
