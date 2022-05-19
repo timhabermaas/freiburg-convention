bf677ae34245928cffc8646438282f110d03e1ef
Tim Habermaas <tim.habermaas@gmail.com>
2022-05-19 23:35:21 +0200

    Move money transfer deadline to Sunday

--- app/domain/app.ts
+++ app/domain/app.ts
@@ -526,7 +526,7 @@ ${ticketLines}
 
 Außerdem hast du uns folgenden Kommentar hinterlassen: ${comment}
 
-Bitte überweise das Geld dafür bis zum 18.05.2022 auf unser Konto:
+Bitte überweise das Geld dafür bis zum 22.05.2022 auf unser Konto:
 
 Empfänger: Jonglieren in Freiburg e.V.
 Bank: Sparkasse Freiburg Nördlicher Breisgau
@@ -551,7 +551,7 @@ ${ticketLines}
 
 You sent us the following comment: ${comment}
 
-Please transfer the money to our account until the 18th of May of 2022:
+Please transfer the money to our account until the 22nd of May of 2022:
 
 Recipient: Jonglieren in Freiburg e.V.
 Bank: Sparkasse Freiburg Nördlicher Breisgau
